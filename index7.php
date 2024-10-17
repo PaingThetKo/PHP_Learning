27586133b3b0e275bf265849f5712633fa440aa7
PaingThetKo <lordpaingthetko@gmail.com>
2024-10-17 12:15:34 +0630

    associated arrays

--- index7.php
+++ index7.php
@@ -1,5 +1,11 @@
 <?php
 
-$person = ["Kyaw Kyaw", 20, "Black"];
+// $person = ["Kyaw Kyaw", 20, "Black"];
+
+$person = [
+    "name" => "Kyaw Kyaw",
+    "age" => 20,
+    "hair" => "Black"
+];
 
 require "index7.view.php";
